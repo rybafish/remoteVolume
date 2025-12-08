911298be5eb872eead95da238675f11479878517
rybafish <rybafishcharts@gmail.com>
2025-12-08 18:22:04 +0100

    sync

--- mix_lin.py
+++ mix_lin.py
@@ -1,19 +1,48 @@
+from pulsectl import Pulse, PulseError
+
 class Mixer:
 
+    pulse = None
     device = None
+    device_id = None
     volume = None
 
     def __init__(self):
         pass
 
     def init(self):
-        pass
+        try:
+            self.pulse = Pulse('get-volume')
+            self.sink = self.pulse.get_sink_by_name(pulse.server_info().default_sink_name)
+        except PulseError as e:
+            print('cannot init pulse')
+
+        self.device_id = self.sink.index
+        print(f'device id: {self.device_id}')
 
     def deviceChanged(self):
-        pass
 
+        if self.sink is None:
+            self.init()
+
+        if self.sink is None:
+            return
+        
+        id = self.sink.index
+        
+
+        if self.device_id is None:
+            self.device_id = id
+            return False
+
+        if self.device_id == id:
+            return False
+
+        return True
+            
     def getVolume(self):
-        pass
+        vol = sum(self.sink.volume.values) / len(self.sink.volume.values)
+        return vol
     
-    def setVolume(self, v):
-        pass
+    def setVolume(self, vol):
+        self.pulse.volume_set_all_chans(self.sink, vol)
